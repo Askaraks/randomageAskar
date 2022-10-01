a8cc0080f1246324f901aa4f409a9536667d8aa7
Askar <114490190+Askaraks@users.noreply.github.com>
2022-10-01 22:50:48 +0600

    Add files via upload

---
+++ generaterandimageAskar/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+            int generateRandomAge = (int) ( Math.random() * 99 );
+            System.out.println("Ваш возраст: " + generateRandomAge);
+    }
+}
